aa8cff7f82b133e7465534b69c201683b024ffbf
kingtw447 <162013545+kingtw447@users.noreply.github.com>
2024-03-04 03:22:56 +0800

    Update and rename lintv.php to lintv.txt

--- lintv.php
+++
@@ -1,5 +0,0 @@
-中视,https://raw.githubusercontent.com/ChiSheng9/iptv/master/TV42.m3u8
-华视,https://raw.githubusercontent.com/ChiSheng9/iptv/master/TV12.m3u8
-台视新闻,https://raw.githubusercontent.com/ChiSheng9/iptv/master/TV25.m3u8
-中视新闻,https://raw.githubusercontent.com/ChiSheng9/iptv/master/TV09.m3u8
-中天新闻,https://raw.githubusercontent.com/ChiSheng9/iptv/master/TV28.m3u8
